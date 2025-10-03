13fc658e6737a90b013dbfd7dbc1da74d66d4c47
Kitkuk <110992990+noname123920@users.noreply.github.com>
2025-10-03 17:02:17 +0500

    Update my_fzero.m

--- my_fzero.m
+++ my_fzero.m
@@ -2,7 +2,7 @@ clear all; clc;
 
 % Твои результаты из предыдущих задач
 x_simple = 0.754861;    % Задача 1
-x_mod = 0.754880;       % Задача 2
+x_mod = 0.754889;       % Задача 2
 x_newton = 0.754878;    % Задача 3
 
 % Эталонный результат fzero
@@ -17,3 +17,4 @@ fprintf('fzero                | %.6f  | -\n', x_fzero);
 fprintf('Простая итерация     | %.6f  | %+.2e\n', x_simple, x_simple - x_fzero);
 fprintf('Мод. итерация        | %.6f  | %+.2e\n', x_mod, x_mod - x_fzero);
 fprintf('Ньютон               | %.6f  | %+.2e\n', x_newton, x_newton - x_fzero);
+
