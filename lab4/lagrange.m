fe70a7d4ace335455efc37c45c6429ca147782ed
Kitkuk <110992990+noname123920@users.noreply.github.com>
2025-12-10 22:01:48 +0500

    /

---
+++ lab4/lagrange.m
@@ -0,0 +1,74 @@
+% Лабораторная работа №4, вариант 5
+% f(x) = 4^(cos(x)), [a,b] = [-3, 0]
+clear; clc; close all;
+
+a = -3;
+b = 0;
+
+%узлы равномерной сетки
+x_nodes = [-3, -2, -1, 0];
+
+% Функция f(x) = 4^(cos(x))
+f = @(x) 4 .^ (cos(x));
+
+y_nodes = f(x_nodes);
+
+
+fprintf("Узлы и значения функции f(x) = 4^(cos(x)):\n");
+for i = 1:length(x_nodes)
+    printf("x_%d = %2d, f(x_%d) = %.6f\n", i-1, x_nodes(i), i-1, y_nodes(i));
+end
+
+% Точки погрешности
+x_j = [(5*a + b)/6, (a + b)/2, (a + 5*b)/6];
+fprintf("\nx_j:");
+disp(x_j);
+y_j = f(x_j);
+fprintf("y_j:");
+disp(y_j);
+
+% Базисные полиномы как функции (вручную считал)
+l0 = @(x) ((x + 2) .* (x + 1) .* x) / (-6);
+l1 = @(x) ((x + 3) .* (x + 1) .* x) / (2);
+l2 = @(x) ((x + 3) .* (x + 2) .* x) / (-2);
+l3 = @(x) ((x + 3) .* (x + 2) .* (x + 1)) / (6);
+
+% Интерполяционный многочлен Лагранжа
+L = @(x) y_nodes(1)*l0(x) + y_nodes(2)*l1(x) + y_nodes(3)*l2(x) + y_nodes(4)*l3(x);
+
+L_xnodes = L(x_nodes);
+fprintf("Значения полинома Лагранжа в узлах:" );
+disp(L_xnodes);
+
+L_xj = L(x_j);
+fprintf("Значения полинома Лагранжа в точках x_j:" );
+disp(L_xj);
+
+errors_L = abs(y_j - L_xj);
+% Вывод таблицы
+fprintf('\nТаблица результатов (равномерная сетка):\n');
+fprintf('-------------------------------------------------------------\n');
+fprintf('j   x_j        f(x_j)      L3(x_j)      |f(x_j) - L3(x_j)|\n');
+fprintf('-------------------------------------------------------------\n');
+for j = 1:length(x_j)
+    fprintf('%d   %6.3f   %10.6f   %10.6f   %10.6e\n', ...
+            j-1, x_j(j), y_j(j), L_xj(j), errors_L(j));
+end
+fprintf('-------------------------------------------------------------\n');
+
+
+
+
+
+xx = linspace(a, b, 200);
+figure;
+plot(xx, f(xx), 'k-', xx, L(xx), 'b--', 'LineWidth', 1);
+hold on;
+plot(x_nodes, y_nodes, 'ro', 'MarkerSize', 5, 'LineWidth', 1);
+hold off;
+grid on;
+legend('f(x)', 'L_3(x)', 'Узлы', 'Location', 'best');
+xlabel('x'); ylabel('y');
+
+
+
---
+++ lab4/newton.m
@@ -0,0 +1,76 @@
+% Узлы те же
+a = -3;
+b = 0;
+
+
+% Функция f(x) = 4^(cos(x))
+f = @(x) 4 .^ (cos(x));
+
+x_nodes = [-3, -2, -1, 0];
+y_nodes = f(x_nodes);
+
+% 1. Разделённые разности
+n = length(x_nodes);
+coeff = y_nodes;  % коэффициенты Ньютона
+for j = 2:n
+    for i = n:-1:j
+        coeff(i) = (coeff(i) - coeff(i-1)) / (x_nodes(i) - x_nodes(i-j+1));
+    end
+end
+
+
+fprintf("Узлы и значения функции f(x) = 4^(cos(x)):\n");
+for i = 1:length(x_nodes)
+    printf("x_%d = %2d, f(x_%d) = %.6f\n", i-1, x_nodes(i), i-1, y_nodes(i));
+end
+
+
+% Вывод коэффициентов
+fprintf('\nКоэффициенты полинома Ньютона:\n');
+for i = 1:n
+    fprintf('a%d = %.6f\n', i-1, coeff(i));
+end
+
+% 2. Функция полинома Ньютона (с поэлементным умножением)
+P = @(x) coeff(1) ...
+       + coeff(2)*(x - x_nodes(1)) ...
+       + coeff(3)*(x - x_nodes(1)).*(x - x_nodes(2)) ...
+       + coeff(4)*(x - x_nodes(1)).*(x - x_nodes(2)).*(x - x_nodes(3));
+
+
+% Точки погрешности
+x_j = [(5*a + b)/6, (a + b)/2, (a + 5*b)/6];
+fprintf("\nx_j:");
+disp(x_j);
+P_xj = P(x_j);
+fprintf("Значения в точках x_j:");
+disp(P_xj);
+
+
+y_j = f(x_j);
+errors_P = abs(y_j - P_xj);
+
+% Вывод таблицы
+fprintf('\nТаблица результатов (равномерная сетка):\n');
+fprintf('-------------------------------------------------------------\n');
+fprintf('j   x_j        f(x_j)      P3(x_j)      |f(x_j) - P3(x_j)|\n');
+fprintf('-------------------------------------------------------------\n');
+for j = 1:length(x_j)
+    fprintf('%d   %6.3f   %10.6f   %10.6f   %10.6e\n', ...
+            j-1, x_j(j), y_j(j), P_xj(j), errors_P(j));
+end
+fprintf('-------------------------------------------------------------\n');
+
+xx = linspace(a, b, 200);
+figure;
+plot(xx, f(xx), 'k-', xx, P(xx), 'b--', 'LineWidth', 1);
+hold on;
+plot(x_nodes, y_nodes, 'ro', 'MarkerSize', 5, 'LineWidth', 1);
+hold off;
+grid on;
+legend('f(x)', 'P_3(x)', 'Узлы', 'Location', 'best');
+xlabel('x'); ylabel('y');
+
+
+
+
